a03b88870467925e8d10e3129708a04e2470813c
dieunn <nndieu88@gmail.com>
2026-04-14 11:18:06 +0700

    up

---
+++ src/main/java/com/demo/projectb/BController.java
@@ -0,0 +1,24 @@
+package com.demo.projectb;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BController {
+
+    @GetMapping("/api/b/data")
+    public ResponseEntity<String> getData(@RequestParam(defaultValue = "ok") String mode) throws InterruptedException {
+        if ("slow".equalsIgnoreCase(mode)) {
+            Thread.sleep(15000);
+            return ResponseEntity.ok("Response from B (slow)");
+        }
+
+        if ("fail".equalsIgnoreCase(mode)) {
+            return ResponseEntity.internalServerError().body("B failed intentionally");
+        }
+
+        return ResponseEntity.ok("Response from B (ok)");
+    }
+}
---
+++ src/main/java/com/demo/projectb/ProjectBApplication.java
@@ -0,0 +1,12 @@
+package com.demo.projectb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBApplication.class, args);
+    }
+}
