693d84b90b789b3d3abce1b87cbc19022f297944
dieunn <nndieu88@gmail.com>
2026-04-14 15:38:14 +0700

    up

--- src/main/java/com/demo/projectb/BController.java
+++ src/main/java/com/demo/projectb/BController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slt
 public class BController {
 
     @GetMapping("/api/b/data")
