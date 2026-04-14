d738f42c2b589982427bb356276b17045c4ae2fb
dieunn <nndieu88@gmail.com>
2026-04-14 15:39:33 +0700

    up

--- src/main/java/com/demo/projectb/BController.java
+++ src/main/java/com/demo/projectb/BController.java
@@ -1,16 +1,18 @@
 package com.demo.projectb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slt
+@Slf4j
 public class BController {
 
     @GetMapping("/api/b/data")
     public ResponseEntity<String> getData(@RequestParam(defaultValue = "ok") String mode) throws InterruptedException {
+        log.info("mode: {}", mode);
         if ("slow".equalsIgnoreCase(mode)) {
             Thread.sleep(15000);
             return ResponseEntity.ok("Response from B (slow)");
